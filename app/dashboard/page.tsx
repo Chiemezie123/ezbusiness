4c5c2cd80fe1a8ecc6a3308df6ccffce52f7a406
nduaguba chiemezie anthony <anthonynduaguba123@gmail.com>
2025-02-24 00:22:00 +0000

    neglected gap

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section className="flex-1 flex flex-col gap-[20.42]">
+      <section className="flex-1 flex flex-col gap-[20.42px]">
         <div className="flex gap-[14.8px]">
           {userDetails.map((items,index) => (
             <SecondDisplayCard
